e7c157a60f9fe73f17f5ef896093ad7b4b5083c9
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-01-22 13:52:28 +0530

    g hub

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+
+const router = express.Router();
+
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+
+router.post('/', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+
+export default router;
---
+++ readme.md
@@ -0,0 +1,12 @@
+This is a single-page application on the frontend using React, Redux, 
+React Router and React Bootstrap. 
+The backend will use Express, MongoDB and Mongoose for the database and will 
+implement authentication using JWT (JSON Web Tokens) and HTTP-only cookies. 
+I used Redux for state management and Redux Toolkit for making things easier.
+
+This app uses the [Vite] Build tool. 
+
+The main focus of this project is to get authentication setup. 
+
+It gives you a good understanding of creating routes/controllers in the api and 
+then managing state in React/Redux.
